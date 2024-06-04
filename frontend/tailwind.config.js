f34897136658c761de39e5a64f7efc21dbe80c5c
havenousername <andrei.cristea997@gmail.com>
2024-06-04 14:48:49 +0200

    fix: add tailwind config

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      'sans': ['Poppins', 'Helvetica', 'Arial', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
